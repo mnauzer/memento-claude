ff114c99a2e700812fd48c8109b1048ab4366ca8
rasto.bakchus <rasto.skubal@gmail.com>
2025-08-20 07:31:34 +0200

    update

--- Dochádzka/Dochádzka Group Summary 3.1.js
+++ Dochádzka/Dochádzka Group Summary 3.1.js
@@ -32,7 +32,7 @@ var CONFIG = {
     
     // Názvy polí v Defaults - UPRAVENÉ PODĽA TVOJICH SCREENSHOTOV
     defaultsFields: {
-        dochadzkaGroupEnabled: "Posielať group notifikácie dochádzky",  // Upravené
+        dochadzkaGroupEnabled: "Dochádzka skupinové dochádzky",  // Upravené
         telegramGroupLink: "Telegram skupina dochádzky",                // Link to Entry pole
         telegramDochadzkaId: "Telegram Dochádzka ID",                   // Textové pole pre spätnú kompatibilitu
         nazovFirmy: "Názov firmy",
