2aaf27b75d50dc9f70087890a7322dab3039d5d4
mnauzer <76249162+mnauzer@users.noreply.github.com>
2025-08-25 18:36:54 +0200

    Update Dochádzka Prepočet 7.js

--- Dochádzka Prepočet 7.js
+++ Dochádzka Prepočet 7.js
@@ -36,6 +36,7 @@
 // Jednoduchý import všetkého cez MementoUtils
 var utils = MementoUtils;
 var config = utils.getConfig();
+var central config = utils.config;
 var currentEntry = entry();
 var CONFIG = {
     // Script špecifické nastavenia
