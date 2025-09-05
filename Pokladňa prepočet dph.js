795468978582be0bf13043ff069d52966f0182df
rasto.bakchus <rasto.skubal@gmail.com>
2025-09-05 10:53:32 +0200

    update

--- Pokladňa prepočet dph.js
+++ Pokladňa prepočet dph.js
@@ -274,6 +274,7 @@ function calculateVat(vatRate) {
         utils.safeSet(currentEntry, CONFIG.fields.sum, finalSumWithoutVat);
         utils.safeSet(currentEntry, CONFIG.fields.sumTotal, finalSumWithVat);
         utils.safeSet(currentEntry, CONFIG.fields.vat, vatAmount);  // Ukladá DPH do poľa "DPH"
+        utils.safeSet(currentEntry, CONFIG.fields.vatRate, vatRate);  // Ukladá sadzba DPH do poľa "sadzba DPH"
         
         utils.addDebug(currentEntry, "  • Suma bez DPH: " + utils.formatMoney(finalSumWithoutVat));
         utils.addDebug(currentEntry, "  • DPH (" + vatRate + "%): " + utils.formatMoney(vatAmount));
