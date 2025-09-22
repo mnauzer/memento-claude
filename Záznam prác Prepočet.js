070ee8e8ca08174cf52c550c060f0349f2731c1c
mnauzer <rasto.skubal+mnauzer@gmail.com>
2025-09-22 16:44:31 +0200

    update

--- Záznam prác Prepočet.js
+++ Záznam prác Prepočet.js
@@ -751,9 +751,9 @@ function processMachines() {
                         //machineryArray[i].setAttr( CONFIG.machinesAttributes.usedMth, usedMth);
                         utils.addDebug(currentEntry, "    ✅ usedMth nastavené");
 
-                        //utils.safeSetAttribute(machineryArray[i], CONFIG.machinesAttributes.totalPrice, totalPrice);
+                        utils.safeSetAttribute(machineryArray[i], CONFIG.machinesAttributes.totalPrice, totalPrice);
 
-                        machineryArray[i].setAttr( CONFIG.machinesAttributes.totalPrice, totalPrice);
+                        //machineryArray[i].setAttr( CONFIG.machinesAttributes.totalPrice, totalPrice);
                         utils.addDebug(currentEntry, "    ✅ totalPrice nastavené");
                     } else {
                         utils.addError(currentEntry, "Nepodarilo sa získať mechanizáciu[" + i + "] z currentEntry", "processMachines");
