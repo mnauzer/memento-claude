938d762767b8c441d57e63a313e35e04004d38e0
mnauzer <rasto.skubal+mnauzer@gmail.com>
2025-09-24 17:17:23 +0200

    update

--- Záznam prác Prepočet.js
+++ Záznam prác Prepočet.js
@@ -673,7 +673,7 @@ function getDefaultHZS() {
             utils.addDebug(currentEntry, "  🔍 Hľadám default HZS v zázname: " + utils.safeGet(defaultEntry, "Účtovný rok", "N/A"));
             utils.addDebug(currentEntry, "  🔍 Nájdených default HZS: " + (defaultHZS ? defaultHZS.length : 0));
             if (defaultHZS && defaultHZS.length > 0) {
-                utils.addDebug(currentEntry, "  ✅ Default HZS nájdené: " + utils.safeGet(defaultHZS[0], "Názov", "N/A"));
+                utils.addDebug(currentEntry, "  ✅ Default HZS nájdené: " + utils.safeGet(defaultHZS[0], "Cena", "N/A"));
                 utils.addDebug(currentEntry, "  " + utils.getIcon("link") + " Default HZS nájdené");
                 return defaultHZS;
             }
